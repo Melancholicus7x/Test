12273de3e84dc6f0caaf2237534fa13db250cf4a
Melancholicus7x <melancholicus7@gmail.com>
2024-11-21 21:22:20 +0000

    Made new project

---
+++ countdown/src/App.jsx
@@ -0,0 +1,17 @@
+import viteLogo from '/vite.svg'
+import './App.css'
+import { useEffect } from 'react'
+
+function App() {
+
+  useEffect(() =>{
+    console.log('hello')
+  }, [])
+
+  return (
+  <>
+  </>
+  )
+}
+
+export default App
